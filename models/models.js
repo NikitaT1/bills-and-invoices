e37cf191b26991a366327e55c042aa7d8acee1a4
NikitaT1 <nik.tati1@gmail.com>
2021-05-14 16:51:56 +0300

    Created models for dataBase. Not sure that its final version, cause maybe I'am gonna rewrite some relations between them later

---
+++ models/models.js
@@ -0,0 +1,33 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const Customer = sequelize.define("customer", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  firstName: { type: DataTypes.STRING },
+  lastName: { type: DataTypes.STRING },
+});
+
+// const RecipientsEmail = sequelize.define("bill", {
+//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//   email: { type: DataTypes.STRING },
+// });
+
+const Recipient = sequelize.define("bill", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  ResipientsEmail___id: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING },
+});
+
+const Bill = sequelize.define("bill", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  workPerformed: { type: DataTypes.STRING },
+  price: { type: DataTypes.INTEGER },
+});
+
+Customer.hasMany(Recipient);
+Recipient.belongsTo(Customer);
+
+Recipient.hasMany(Bill);
+Bill.belongsTo(Recipient);
+
+module.exports = { Customer, Recipient, Bill };
