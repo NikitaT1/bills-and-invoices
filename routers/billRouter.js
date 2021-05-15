cc2bd86fce4a7f7724d29a551938fcfbf743759c
NikitaT1 <nik.tati1@gmail.com>
2021-05-15 19:16:42 +0300

    Haven't getting data from databas and skipped point 3 of the task, gonna make it later. Added html-pdf-node library

--- routers/billRouter.js
+++ routers/billRouter.js
@@ -1,12 +1,20 @@
 const Router = require("express");
 const router = new Router();
 const { Bill } = require("../models/models");
+const html_to_pdf = require("html-pdf-node");
 
-router.post("/", async (req, res) => {
-  const { workPerformed, price } = req.body;
+router.get("/", async (req, res) => {
+  //const { workPerformed, price } = req.body;
   try {
-    const type = await Bill.create({ workPerformed, price });
-    return res.json(type);
+    // const type = await Bill.create({ workPerformed, price });
+
+    let options = { format: "A4" };
+    let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
+    html_to_pdf.generatePdf(file, options).then((pdfBuffer) => {
+      res.file(pdfBuffer);
+    });
+
+    //return res.json(type);
   } catch (er) {
     console.log(er);
   }
