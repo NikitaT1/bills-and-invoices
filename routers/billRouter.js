e1f8b1ff64db5b4b7748e04bde6af6bf69515fc6
NikitaT1 <nik.tati1@gmail.com>
2021-05-15 21:23:45 +0300

    Added Mailgun. It doesn't afford to publish api-keys on github and some how there is a bug when a put them into env, so it's just empty string instead of keys, temporary. But it's working on my local machine. Also added logic for attaching PDF files. Now code looks not quite good, I'am gonna refactor and make it clean later

--- routers/billRouter.js
+++ routers/billRouter.js
@@ -2,16 +2,37 @@ const Router = require("express");
 const router = new Router();
 const { Bill } = require("../models/models");
 const html_to_pdf = require("html-pdf-node");
+const mailgun = require("mailgun-js");
 
-router.get("/", async (req, res) => {
-  //const { workPerformed, price } = req.body;
+const DOMAIN = "_";
+const mg = mailgun({
+  apiKey: "_",
+  domain: DOMAIN,
+});
+
+router.post("/", async (req, res) => {
+  const { workPerformed, price } = req.body;
   try {
-    // const type = await Bill.create({ workPerformed, price });
+    const type = await Bill.create({ workPerformed, price });
 
     let options = { format: "A4" };
     let file = { content: "<h1>Welcome to html-pdf-node</h1>" };
     html_to_pdf.generatePdf(file, options).then((pdfBuffer) => {
-      res.file(pdfBuffer);
+      var attch = new mg.Attachment({
+        data: pdfBuffer,
+        filename: "filename",
+        contentType: "application/pdf",
+      });
+      const data = {
+        from: "Excited User <me@samples.mailgun.org>",
+        to: "nik.tati1@gmail.com, YOU@YOUR_DOMAIN_NAME",
+        subject: "Hello",
+        text: "Testing some Mailgun awesomness!!!!!!",
+        attachment: [attch],
+      };
+      mg.messages().send(data, function (error, body) {
+        console.log(body);
+      });
     });
 
     //return res.json(type);
