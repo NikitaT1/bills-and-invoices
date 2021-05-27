63a5acf2353016fbbd533f29e6e0bb13f65ced15
NikitaT1 <nik.tati1@gmail.com>
2021-05-27 14:56:34 +0300

    Uncomment code for logging requests

--- index.js
+++ index.js
@@ -10,24 +10,14 @@ const swaggerUi = require("swagger-ui-express"),
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-//app.use(logger);
+app.use((req, res, done) => {
+  logger.info(req.originalUrl);
+  done();
+});
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// io.subscribe("my-channel-1", "my-channel-2", (err, count) => {
-//   if (err) {
-//     // Just like other commands, subscribe() can fail for some reasons,
-//     // ex network issues.
-//     console.error("Failed to subscribe: %s", err.message);
-//   } else {
-//     // `count` represents the number of channels this client are currently subscribed to.
-//     console.log(
-//       `Subscribed successfully! This client is currently subscribed to ${count} channels.`
-//     );
-//   }
-// });
 
 const start = async () => {
   try {
--- logger/logger.js
+++ logger/logger.js
@@ -1,59 +1,7 @@
 const winston = require("winston");
-const path = require("path");
 
-// const logger = winston.createLogger({
-//   level: "http",
-//   format: winston.format.simple(),
-//   //defaultMeta: { service: "user-service" },
-//   transports: [
-//     new winston.transports.Console(),
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+});
 
-//     new winston.transports.File({ filename: "http.log", level: "http" }),
-//   ],
-// });
-
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(
-//     new winston.transports.Console({
-//       format: winston.format.simple(),
-//     })
-//   );
-// }
-module.exports = () => {
-  process.on("uncaughtException", (err) =>
-    winston.error("uncaught exception: ", err)
-  );
-  process.on("unhandledRejection", (reason, p) =>
-    winston.error("unhandled rejection: ", reason, p)
-  );
-
-  winston.exitOnError = false;
-  winston.level = process.env.NODE_ENV === "production" ? "info" : "debug";
-  winston.remove(winston.transports.Console);
-
-  // winston.add(winston.transports.Console, {
-  //   level: "debug",
-  //   handleExceptions: true,
-  //   prettyPrint: true,
-  //   humanReadableUnhandledException: false,
-  //   json: false,
-  //   colorize: true,
-  //   timestamp: new Date(),
-  // });
-
-  // winston.add(winston.transports.File, {
-  //   // filename: "http.log",
-  //   level: "http",
-  //   level: "debug",
-  //   filename: path.join(__dirname, "./app_asd.log"),
-  //   handleExceptions: true,
-  //   humanReadableUnhandledException: true,
-  //   json: false,
-  //   maxsize: 10242880, // ~10MB
-  //   maxFiles: 3,
-  //   colorize: false,
-  //   timestamp: new Date(),
-  // });
-
-  winston.add(new winston.transports.File({ filename: "logfile.log" }));
-};
+module.exports = logger;
