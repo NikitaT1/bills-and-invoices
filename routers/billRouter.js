882ec2e4f63546cfc11732c9799fbcb9c8fd393b
NikitaT1 <nik.tati1@gmail.com>
2021-06-07 15:31:39 +0300

    Huge update. I removed all blocks of code for creating pdf and sending e-mail - it's only in separate microservice app now and I guess these part of the app finally looks Ok, but there are still a lot of things to refactor

--- routers/billRouter.js
+++ routers/billRouter.js
@@ -3,9 +3,7 @@ import customValidation from "../middleware/validation";
 import billSchema from "../schema/billSchema";
 import config from "../config";
 import { Queue } from "bullmq";
-import mailWorker from "../sendEmail/mailWorker";
 import dbWorker from "./billWorker";
-import htmlWorker from "../htmlPdfGenerator/htmlWorker";
 const Redis = require("ioredis");
 const io = new Redis(); // Connect to 127.0.0.1:6379
 
@@ -17,29 +15,12 @@ const dbQueue = new Queue(config.queueName1, {
   connection: config.connection,
 });
 
-const htmlQueue = new Queue(config.queueName2, {
-  connection: config.connection,
-});
-
-const mailQueue = new Queue(config.queueName3, {
-  connection: config.connection,
-});
-
 router.post("/", billSchema, customValidation, async (req, res) => {
   let resData = {};
   try {
     dbWorker.on("completed", async (result) => {
       resData = { ...result.returnvalue };
-      await htmlQueue.add("someTaskName", result.returnvalue);
-      io.publish("my-channel-1", JSON.stringify(result.returnvalue));
-    });
-
-    htmlWorker.on("completed", async () => {
-      await mailQueue.add("someTaskName");
-    });
-
-    mailWorker.on("completed", async (job) => {
-      res.send(resData);
+      io.publish("createAndSendInvoice", JSON.stringify(result.returnvalue));
     });
 
     await dbQueue.add("dataBase in quene", req.body);
--- sendEmail/mailWorker.js
+++
@@ -1,8 +0,0 @@
-import { Worker } from "bullmq";
-import { connection, queueName3 } from "../config";
-
-const mailWorker = new Worker(queueName3, `${__dirname}/sendEmail.js`, {
-  connection,
-});
-
-export default mailWorker;
